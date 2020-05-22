c38bd3de6f13a6c8e4f5e5ead89433a942627bed
Sophia Li <sophiali124@gmail.com>
2020-05-22 01:52:05 -0700

    update env for netlify

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const url = "https://api.nasa.gov/planetary/apod?api_key=";
-const api_key = config.NASA_API_KEY;
+// const api_key = config.NASA_API_KEY;
+const api_key = process.env.NASA_API_KEY;
 
 const fetchNASAData = async () => {
   try {
